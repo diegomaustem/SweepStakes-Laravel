f8eb0ea4adead6c24652c4cf11809e617a45334f
maustem <diego.maustem@gmail.com>
2024-10-11 11:40:58 -0300

    :sparkles: feat: Instaling fortify and configurations of routes fortify

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class ProjectsController extends Controller
 {
-    public function index()
-    {
-        return view('projects.index');
-    }
+    // public function login()
+    // {
+    //     return view('auth.index');
+    // }
+
+    // public function register()
+    // {
+
+    // }
 }
